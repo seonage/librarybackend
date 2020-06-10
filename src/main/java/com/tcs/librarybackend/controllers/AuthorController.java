7bb430dffae084275f76d658cdcd6fa3a270597b
Steven Lin <steven.lin1986@gmail.com>
2020-06-10 03:53:55 -0400

    Create route for retrieving all Authors from database

---
+++ src/main/java/com/tcs/librarybackend/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package com.tcs.librarybackend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tcs.librarybackend.repos.AuthorRepo;
+import com.tcs.librarybackend.models.Author;
+
+@RestController
+@RequestMapping("/authors")
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthorController {
+    @Autowired
+    private AuthorRepo authorRepo;
+
+    @GetMapping
+    public List<Author> getAuthors() {
+        return authorRepo.findAll();
+    }
+}
--- src/main/java/com/tcs/librarybackend/repos/AuthorRepo.java
+++ src/main/java/com/tcs/librarybackend/repos/AuthorRepo.java
@@ -4,6 +4,7 @@ import com.tcs.librarybackend.models.Author;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@RepositoryRestResource(collectionResourceRel = "authors", path = "authors")
 public interface AuthorRepo extends MongoRepository<Author, String> {
 
 }
