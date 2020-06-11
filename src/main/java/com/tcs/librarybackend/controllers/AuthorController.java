d09b80839857b68c37790de7107a3f52ef51c9d2
Steven Lin <steven.lin1986@gmail.com>
2020-06-11 01:40:07 -0400

    Added routers for searching for a specific author and for deleting an author from database

--- src/main/java/com/tcs/librarybackend/controllers/AuthorController.java
+++ src/main/java/com/tcs/librarybackend/controllers/AuthorController.java
@@ -3,15 +3,20 @@ package com.tcs.librarybackend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.tcs.librarybackend.repos.AuthorRepo;
 import com.tcs.librarybackend.models.Author;
+import com.tcs.librarybackend.exceptions.ResourceNotFoundException;
 
 @RestController
 @RequestMapping("/authors")
@@ -25,9 +30,21 @@ public class AuthorController {
         return authorRepo.findAll();
     }
 
+    @GetMapping(value="/{id}")
+    public Author getOneAuthor(@PathVariable String id){
+        return authorRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+    }
+
     @PostMapping
     public void addAuthor(@RequestBody Author author) {
         authorRepo.save(author);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public void deleteAuthor(@PathVariable String id) {
+        Author author = authorRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+        authorRepo.delete(author);
     }
 
 }
