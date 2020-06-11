7078fdd97efa017c0f7f176540628fdc3b3da654
Steven Lin <steven.lin1986@gmail.com>
2020-06-11 01:32:30 -0400

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/tcs/librarybackend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tcs.librarybackend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException  extends RuntimeException {
+    public ResourceNotFoundException() {
+
+    }
+}
